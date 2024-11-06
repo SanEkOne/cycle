3fd1f506feb51ed31a5c97710be2ffbbc5afa399
Ostap_yd89 <Ostap_yd89@21-2>
2024-11-06 18:57:33 +0200

    Add project files.

---
+++ cycle/cycle.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    for (int i = 0; i <= 100; i++) {
+        if (i % 3 == 0) {
+            cout << i << endl;
+        }
+    }
+}
+
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    for (int i = 0; i <= 100; i++) {
+        if (i % 5 == 0) {
+            cout << i << endl;
+        }
+    }
+}
+
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    for (int i = 0; i <= 100; i++) {
+        if (i % 7 == 0) {
+            cout << i << endl;
+        }
+    }
+}
